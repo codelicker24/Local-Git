a629b4636996ec892be4f596031a2d12d95ac064
Sathuluri Anoushka <anoushka242006@gmail.com>
2024-12-25 16:11:49 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+// 1. Hello World
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// 2. Variables and Data Types
+public class VariablesExample {
+    public static void main(String[] args) {
+        // Primitive data types
+        int age = 25;
+        double salary = 50000.50;
+        boolean isStudent = true;
+        char grade = 'A';
+        
+        // String (Reference type)
+        String name = "John Doe";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Is Student? " + isStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
+
+// 3. Basic Operators
+public class OperatorsExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        // Arithmetic operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
